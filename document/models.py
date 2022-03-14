29d54d602675cd36016d18bbd64b1b7f9b6f3dad
Kaiyrly <kayrasktl@gmail.com>
2022-03-14 14:50:04 +0900

    added post and delete functions

--- document/models.py
+++ document/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
-class  Document(models.Model):
+class Document(models.Model):
     title = models.CharField(max_length = 255)
     content = models.TextField(blank = True, null = True)
+
+    created_at = models.TimeField(auto_now_add = True)
+    modififed_at = models.DateField(auto_now=True)
 
     class Meta:
         ordering = ('title',)
